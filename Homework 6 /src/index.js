ccdb342345f56883cd80581aef7379e79924d476
Areyes9 <98775200+Areyes9@users.noreply.github.com>
2022-03-02 11:57:55 -0500

    Added project

---
+++ Homework 6 /src/index.js
@@ -0,0 +1,80 @@
+//Date and Time
+let now = new Date();
+
+let date = now.getDate();
+let minutes = now.getMinutes();
+let hour = now.getHours();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+let time = document.querySelector("#date");
+time.innerHTML = `${day} ${date}, ${hour}:${minutes}`;
+
+// City
+function showTemperature(response) {
+  document.querySelector("#city-search").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  let description = document.querySelector("#temp-description");
+  description.innerHTML = response.data.weather[0].description;
+}
+
+function search(event) {
+  event.preventDefault();
+  let apiKey = "147c7ccb0d8865155667a7334b1e39df";
+  let city = document.querySelector("#search-text-input").value;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(showTemperature);
+}
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", search);
+
+//Farenheit to Celsius
+function convertToCelsius(event) {
+  event.preventDefault();
+
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = temperatureElement.innerHTML;
+  temperature = Number(temperature);
+  temperatureElement.innerHTML = 66;
+}
+
+function convertToFarenheit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = 19;
+}
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", convertToCelsius);
+
+let farenheitLink = document.querySelector("#farenheit-link");
+farenheitLink.addEventListener("click", convertToFarenheit);
+
+//Search Engine HM 5
+let apiKey = "147c7ccb0d8865155667a7334b1e39df";
+let city = "Arlington";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+
+//Current Location Button
+function searchLocation(position) {
+  let apiKey = "147c7ccb0d8865155667a7334b1e39df";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(showTemperature);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
