6c0efb4eebf78afba74c983eb42816c6d5e75e5e
Areyes9 <98775200+Areyes9@users.noreply.github.com>
2022-04-01 17:13:37 -0400

    Added api call for forecast

--- src/index.js
+++ src/index.js
@@ -32,6 +32,44 @@ let month = months[now.getMonth()];
 let time = document.querySelector("#date");
 time.innerHTML = `${day} ${month} ${date}, ${hour}:${minutes}`;
 
+function displayForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Fri", "Sat", "Sun", "Mon"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+            <div class="col-4">
+              <div class="forecast-day">${day}</div>
+            </div>
+            <div class="col-4">
+              <img
+                src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+                id="icon"
+                width="40"
+              />
+            </div>
+            <div class="col-4">
+              <div class="forecast-temp">
+                <span class="forecast-max">49°F</span>
+                <span class="forecast-min">28°F</span>
+              </div>
+            </div>
+            `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "147c7ccb0d8865155667a7334b1e39df";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 // City
 function showTemperature(response) {
   document.querySelector("#city-search").innerHTML = response.data.name;
@@ -51,6 +89,8 @@ function showTemperature(response) {
   iconElement.setAttribute("src", `icons/${response.data.weather[0].icon}.png`);
 
   fahrenheitTemperature = response.data.main.temp;
+
+  getForecast(response.data.coord);
 }
 
 function search(event) {
@@ -99,6 +139,7 @@ let fahrenheitLink = document.querySelector("#f-link");
 fahrenheitLink.addEventListener("click", convertToFahrenheit);
 
 searchCity("Arlington");
+displayForecast();
 
 //Search Engine HM 5
 let apiKey = "147c7ccb0d8865155667a7334b1e39df";
